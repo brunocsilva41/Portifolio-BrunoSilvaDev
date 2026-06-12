f39d932ab4ddeeb9c65d8e400ea5dfde82a53e6f
brunocsilva41 <ybrunooh@gmail.com>
2026-06-12 13:22:52 -0300

    chore: initial project setup with Vite and Vercel config

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/three')) {
+            return 'three';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 300,
+  },
+});
